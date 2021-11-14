43713e4c70e5d7da34224c954cd1daef6d468a70
Dawid Majchrzak <programista.dawid.majchrzak@gmail.com>
2021-11-14 22:14:07 +0100

    Commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+import logo from './images/PRINSBUD-logo.png';
+
+import About from './Routes/About';
+import Gallery from './Routes/Gallery';
+import Contact from './Routes/Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <div className="logo-container">
+          <img src={logo} alt="logo" />
+        </div>
+        <nav>
+          <NavLink exact to='/'>o nas</NavLink>
+          <NavLink to='/galeria'>galeria</NavLink>
+          <NavLink to='/kontakt'>kontakt</NavLink>
+        </nav>
+        <Routes>
+          <Route exact path='/' element={<About />} />
+          <Route path='/galeria' element={<Gallery />} />
+          <Route path='/kontakt' element={<Contact />} />
+        </Routes>
+      </div>
+      <footer>
+        <p>© 2021 PRINSBUD</p>
+      </footer>
+    </BrowserRouter >
+  );
+}
+
+export default App;
